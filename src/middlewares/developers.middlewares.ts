8251e1ebbf29d000cc6bd1ba4b672a90d7ac25bf
Edmilson <krakadianbill@gmail.com>
2023-02-12 23:38:26 -0300

    feat: middlewares now are exported with index

--- src/middlewares/developers.middlewares.ts
+++ src/middlewares/developers.middlewares.ts
@@ -43,8 +43,92 @@ const validateEmailReq = async (req: Request, resp: Response, next: NextFunction
     return next();
 };
 
+const ensureDevExists = async (req: Request, resp: Response, next: NextFunction): Promise<Response | void> =>{
+    const devId: number = +req.params.id;
+    const queryString = 
+    `
+        SELECT
+            *
+        FROM
+            developers
+        WHERE
+            "devId" = $1
+        ;
+    `;
+    const queryConfig: QueryConfig = {
+        text: queryString,
+        values: [devId]
+    };
+    const queryResult: DeveloperResult = await client.query(queryConfig);
+    if(!queryResult.rowCount){
+        return resp.status(404).json({
+            message: "Dev not Found"
+        })
+    };
+    return next();
+};
+
+const validateDevInfoDevReq = async (req: Request, resp: Response, next: NextFunction): Promise<Response | void> =>{
+    const requiredKeys = [ "devInfoDevSince", "devInfoPreferredOS" ];
+    const actualKeys = Object.keys(req.body);
+    const actualEnumValue: string = req.body.devInfoPreferredOS;
+    let validate = actualKeys.every(key => requiredKeys.includes(key));
+
+    if(actualEnumValue !== "Windows" && actualEnumValue !== "Linux" && actualEnumValue !== "MacOS"){
+        return resp.status(400).json({
+            message: "Please verify your Enum request."
+        })
+    };
+    if(!validate){
+        return resp.status(400).json({
+            message: "Verify your request, required keys are:",
+            keys: ["devInfoDevSince", "devInfoPreferredOS"]
+        })
+    };
+    return next();
+};
+
+const patchDevFilter =  async (req: Request, resp: Response, next: NextFunction): Promise<Response | void> =>{
+    const validatedKeys = [ "devName", "devEmail" ];
+    const actualReq: any = req.body;
+    Object.keys(actualReq).forEach(key => {
+        if(!validatedKeys.includes(key)) {
+            delete actualReq[key]
+        }
+    });
+    let validate = Object.keys(actualReq).some(key => validatedKeys.includes(key));
+    if(!validate){
+        return resp.status(400).json({
+            message: "At least one of those keys must be send:",
+            keys: [ "devName","", "devEmail" ]
+        })
+    };
+    next();
+};
+
+const patchDevInfoFilter =  async (req: Request, resp: Response, next: NextFunction): Promise<Response | void> =>{
+    const validatedKeys = [ "devInfoDevSince", "devInfoPreferredOS" ];
+    const actualReq: any = req.body;
+    Object.keys(actualReq).forEach(key => {
+        if(!validatedKeys.includes(key)) {
+            delete actualReq[key]
+        }
+    });
+    let validate = Object.keys(actualReq).some(key => validatedKeys.includes(key));
+    if(!validate){
+        return resp.status(400).json({
+            message: "At least one of those keys must be send.",
+            keys: [ "devInfoDevSince", "", "devInfoPreferredOS" ]
+        })
+    };
+    next();
+};
+
 export{
     validateDevReq,
     validateEmailReq,
-    
-}
+    ensureDevExists,
+    validateDevInfoDevReq,
+    patchDevFilter,
+    patchDevInfoFilter
+}   
